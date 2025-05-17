0cafae2548d3d27f32f058d4e4c1d8fc001fd97f
nadaeltorgoman <nada.khalid.eltorgoman@gmail.com>
2025-05-17 19:47:27 +0300

    add another status to snackBar

--- lib/core/widgets/dialog.dart
+++ lib/core/widgets/dialog.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:taskati/core/utils/app_colors.dart';
 import 'package:taskati/core/utils/text_styles.dart';
 
-showErrorSnackBar(BuildContext context, String message) {
+enum DialogType { success, error, warning }
+
+showErrorSnackBar(
+  BuildContext context,
+  String message, {
+  DialogType type = DialogType.error,
+}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Text(message, style: AppTextStyles.body(color: AppColors.white)),
-      backgroundColor: AppColors.redColor,
+      backgroundColor:
+          type == DialogType.error
+              ? AppColors.redColor
+              : (type == DialogType.warning
+                  ? AppColors.warningColor
+                  : AppColors.successColor),
       duration: Duration(seconds: 2),
     ),
   );
